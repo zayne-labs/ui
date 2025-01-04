fd6c15bc3abdc3697b886a42cb7a99f5a0d385dd
Ryan-Zayne <phoenixastro97@gmail.com>
2025-01-04 20:56:11 +0100

    fix

--- packages/ui-react/src/components/ui/card/card.tsx
+++ packages/ui-react/src/components/ui/card/card.tsx
@@ -1,3 +1,5 @@
+import * as React from "react";
+
 import { Slot } from "@/components/common/slot";
 import { cnMerge } from "@/lib/utils/cn";
 import type { PolymorphicProps } from "@zayne-labs/toolkit-react/utils";
