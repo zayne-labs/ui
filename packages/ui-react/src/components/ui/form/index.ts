d24d1aeb790d4cc30c5c0fce76997ef124434665
Ryan-Zayne <phoenixastro97@gmail.com>
2025-03-26 14:08:02 +0100

    feat: export export formRootContext

--- packages/ui-react/src/components/ui/form/index.ts
+++ packages/ui-react/src/components/ui/form/index.ts
@@ -2,4 +2,6 @@ export * from "./form";
 
 export * as Form from "./form-parts";
 
+export { useFormContext as useFormRootContext } from "react-hook-form";
+
 export { useStrictFormFieldContext as useFormFieldContext } from "./form-context";
