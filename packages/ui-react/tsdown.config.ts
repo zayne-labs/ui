e2501b7879d14978271c9e2250cf612fc9f771a9
Ryan-Zayne <phoenixastro97@gmail.com>
2025-08-23 22:09:43 +0100

    chore: downgrade tsdown due to build errors

--- packages/ui-react/tsdown.config.ts
+++ packages/ui-react/tsdown.config.ts
@@ -1,10 +1,10 @@
-import { type Options, defineConfig } from "tsdown";
+import { defineConfig, type Options } from "tsdown";
 
 const isDevMode = process.env.NODE_ENV === "development";
 
 const sharedOptions = {
 	clean: true, // clean up dist folder,
-	dts: true, // generate d.ts
+	dts: { newContext: true }, // generate d.ts
 	entry: [
 		// UI components
 		"src/components/ui/index.ts",
